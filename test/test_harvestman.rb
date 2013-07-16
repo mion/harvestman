9ac275f8eb582445a09b09becd57aa74201f986b
Gabriel Vieira <gluisvieira@gmail.com>
2013-07-16 18:24:27 -0300

    Tests are failing because of a missing feature.

--- test/test_harvestman.rb
+++ test/test_harvestman.rb
@@ -16,16 +16,18 @@ class TestHarvestman < Test::Unit::TestCase
 				:list => []
 			}
 
-			css("div.main ul") do
+			css "div.main ul" do
 				r[:list] << css("li")
 			end
 
 			results << r
 		end
 
-		results.each_with_index do |result, i|
-			assert_equal(result[:title], "ex#{i+1}")
-			assert_equal(result[:header], "#{result[:title]}_header_h1")
+		results.each_with_index do |r, i|
+			assert_equal(r[:title], "ex#{i+1}")
+			assert_equal(r[:header], "#{r[:title]}_header_h1")			
+			assert_equal(r[:footer], "#{r[:title]}_footer_span_a")
+			assert_equal(r[:list].count, 3)
 		end
 	end
 end
