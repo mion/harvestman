faae081a7d60f80aaa8704a74f411efc32ebc47f
Gabriel Vieira <gluisvieira@gmail.com>
2015-10-10 15:55:48 -0300

    Rewrite to use a simple worker pool

--- lib/harvestman/crawler/fast.rb
+++ lib/harvestman/crawler/fast.rb
@@ -1,3 +1,5 @@
+require 'thread'
+
 module Harvestman
   module Crawler
     class Fast < Base
@@ -5,11 +7,32 @@ module Harvestman
         if @pages.nil?
           crawl_url(@base_url, &block)
         else
-          threads = []
-          @pages.each do |p|
-            threads << Thread.new(p) { |page| crawl_url(@base_url.gsub('*', p.to_s), &block) }
+          # started_at = Time.now
+          urls = @pages.map { |p| @base_url.gsub('*', p.to_s) }
+          work_q = Queue.new
+          urls.each { |url| work_q << url }
+          workers = (0...5).map do
+            Thread.new do
+              begin
+                while url = work_q.pop(true)
+                  begin
+                    crawl_url(url, &block)
+                  rescue => e
+                    puts "[!] Error in URL: #{url}"
+                    puts e.inspect
+                  end
+                end
+              rescue ThreadError
+              end
+            end
           end
-          threads.each { |t| t.join }
+          workers.map(&:join)
+
+          # threads = []
+          # @pages.each do |p|
+          #   threads << Thread.new(p) { |page| crawl_url(@base_url.gsub('*', p.to_s), &block) }
+          # end
+          # threads.each { |t| t.join }
         end
       end
     end
